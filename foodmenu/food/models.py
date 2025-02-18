a39a3ce4de19ef8c97daa88dbbb713248e54b43f
AnchalMakhijani <anchalmakhijani22@gmail.com>
2025-02-18 11:19:55 +0530

    18/02/2025

--- foodmenu/food/models.py
+++ foodmenu/food/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
+
+    def __str__(self):
+        return self.itemname
+
     itemname = models.CharField(max_length=200)
     itemdesc = models.CharField(max_length=200)
     itemprice = models. IntegerField()
